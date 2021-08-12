8bd74efdbe87c1e23cf3ab5eadc1ec5e4ccf287d
JakubSzczerba <jakszczerb@gmail.com>
2021-08-12 13:25:28 +0200

    new file -> Context and I created clas TimeBasedChannelContext

---
+++ src/Context/TimeBasedChannelContext.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Context;
+
+final class TimeBasedChannelContext implements ChannelContextInterface
+{
+
+    public function getChannel(): ChannelInterface
+    {
+        
+    }
+
+}
