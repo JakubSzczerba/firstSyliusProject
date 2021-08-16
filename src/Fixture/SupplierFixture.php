646b4e63e83489f6e21c09f5eede6ddcb886263e
JakubSzczerba <jakszczerb@gmail.com>
2021-08-16 11:26:26 +0200

    writing fixtures

--- src/Fixture/SupplierFixture.php
+++ src/Fixture/SupplierFixture.php
@@ -5,8 +5,18 @@ declare(strict_types=1);
 namespace App\Fixture;
 
 use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
+use Sylius\Bundle\FixturesBundle\Fixture\FixtureInterface;
 
-final class SupplierFixture extends AbstractFixture
+final class SupplierFixture extends AbstractFixture implements FixtureInterface
 {
+    public function load(array $options): void 
+    {
+
+    }
+
+    public function getName(): string 
+    {
+
+    }
 
 }
