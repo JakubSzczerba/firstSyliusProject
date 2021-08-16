3012be8f9cea1290dad94734efda28c7b32c2eba
JakubSzczerba <jakszczerb@gmail.com>
2021-08-16 11:42:11 +0200

    added all needed options and properties to supplier fixtures

--- src/Fixture/SupplierFixture.php
+++ src/Fixture/SupplierFixture.php
@@ -4,18 +4,44 @@ declare(strict_types=1);
 
 namespace App\Fixture;
 
+use Faker\Generator;
+use Doctrine\Common\Persistence\ObjectManager;
 use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
 use Sylius\Bundle\FixturesBundle\Fixture\FixtureInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 
 final class SupplierFixture extends AbstractFixture implements FixtureInterface
 {
+    /** @var FactoryInterface */
+    private $supplierFactory;
+
+    /** @var ObjectManager */
+    private $supplierManager;
+
+    /** @var Generator */
+    private $generator;
+
+
     public function load(array $options): void 
     {
+        for ($i = 0; $i < $options['count']; $i++) {
+
+        }
 
     }
 
     public function getName(): string 
     {
+        return 'supplier';
+    }
+
+    protected function configureOptionsNode(ArrayNodeDefinition $optionsNode): void
+    {
+        $optionsNode
+            ->children()
+            ->integerNode('count')
+            ;
 
     }
 
