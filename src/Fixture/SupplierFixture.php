7420b62c14eda0a42202ed0ed9b1e4fc6bb09b06
JakubSzczerba <jakszczerb@gmail.com>
2021-08-16 11:23:34 +0200

    create a class for suppliers's fixtures

---
+++ src/Fixture/SupplierFixture.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Fixture;
+
+use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
+
+final class SupplierFixture extends AbstractFixture
+{
+
+}
