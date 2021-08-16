365ea663ee6727b7cbaa88fb9b9ae0cb4b5c2b17
JakubSzczerba <jakszczerb@gmail.com>
2021-08-16 20:03:57 +0200

    update of product, new field added -> supplier -> it is a 2nd part of assosations.

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use App\Entity\SupplierInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
@@ -14,8 +15,30 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  */
 class Product extends BaseProduct
 {
+    /**
+     * @var SupplierInterface
+     * 
+     * @ORM\ManyToOne(targetEntity="App\Entity\Supplier", inversedBy="products")
+     * @ORM\JoinColumn(name="supplier_id", referencedColumnName="id")
+     */
+    private $supplier;
+
+    public function getSupplier(): SupplierInterface
+    {
+        return $this->supplier;
+    }
+
+    public function setSupplier(SupplierInterface $supplier): void
+    {
+        $this->supplier = $supplier;
+    }
+    
+    
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
     }
+
+   
 }
