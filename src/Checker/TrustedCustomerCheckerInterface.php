7b6d8ce63e99bf084b3c3016ac1dedd55011dd95
JakubSzczerba <jakszczerb@gmail.com>
2021-08-17 12:19:20 +0200

    fast refactoring -> checker and everything is working :)

---
+++ src/Checker/TrustedCustomerCheckerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Checker;
+
+use Sylius\Component\Core\Model\CustomerInterface;
+
+interface TrustedCustomerCheckerInterface
+{
+    public function check(CustomerInterface $customer): bool; 
+    
+}
