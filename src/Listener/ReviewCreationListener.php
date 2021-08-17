65e71285a0c223c71a5c938009fda0b6e000a8b3
JakubSzczerba <jakszczerb@gmail.com>
2021-08-17 11:42:21 +0200

    all logic for review creation listener

--- src/Listener/ReviewCreationListener.php
+++ src/Listener/ReviewCreationListener.php
@@ -4,9 +4,38 @@ declare(strict_types=1);
 
 namespace App\Listener;
 
-use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use SM\Factory\FactoryInterface;
+use Sylius\Component\Review\Model\ReviewInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\ProductReviewTransitions;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 final class ReviewCreationListener 
 {
+    /** @var FactoryInterface */
+    private $stateMachineFactory;
+
+    public function __construct(FactoryInterface $stateMachineFactory)
+    {
+        $this->stateMachineFactory = $stateMachineFactory;
+    }
+
+    public function acceptForTrustedAuthor(GenericEvent $event): void 
+    {
+        /** @var ReviewInterface $review */
+        $review = $event->getSubject();
+
+        /** @var CustomerInterface $author */
+        $author = $review->getAuthor();
+
+        if ($author->getGroup() === null || $author->getGroup()->getCode() !== 'TRUSTED') {
+            return;
+
+        }
+
+        $stateMachine = $this->stateMachineFactory->get($review, ProductReviewTransitions::GRAPH);
+        $stateMachine->apply(ProductReviewTransitions::TRANSITION_ACCEPT);
+
+    }
 
 }
